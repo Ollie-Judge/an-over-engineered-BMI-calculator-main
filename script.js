c8df4e11df10e5821935f5477b097c864bf0ed49
Ollie-Judge <ollie.judge@hotmail.com>
2023-06-30 14:35:32 +0100

    added multiple choice functionality for units

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
-const start = () => {
-  const nameInput = document.getElementById("name").value;
-  const heightInput = document.getElementById("height").value;
-  const weightInput = document.getElementById("weight").value;
+const chooseUnits = document.getElementById("chooseUnits");
 
-  let outputDiv = document.getElementById("outputDiv");
+const nameInput = document.getElementById("name");
+const heightInput = document.getElementById("height");
+const weightInput = document.getElementById("weight");
 
-  document.getElementById("resultExplanation").remove();
-  class Person {
+let heightDiv = document.getElementById("heightDiv");
+
+let submitButton = document.getElementById("submitButton");
+
+let outputDiv = document.getElementById("outputDiv");
+
+console.log("test1 works");
+
+chooseUnitsChange = () => {
+  if (chooseUnits.value === "Metric") {
+    console.log(chooseUnits.value);
+
+    heightInput.placeholder = "Height in m";
+    document.getElementById("weight").placeholder = "Weight in kg";
+
+    submitButton.addEventListener("click", startMetric);
+  } else if (chooseUnits.value === "Imperial") {
+    console.log(chooseUnits.value);
+
+    heightInput.placeholder = "ft";
+    weightInput.placeholder = "Weight in lbs";
+
+    let inchesInput = document.createElement("input");
+    inchesInput.setAttribute("type", "number");
+    inchesInput.setAttribute("id", "inches");
+    inchesInput.setAttribute("placeholder", "inches");
+    inchesInput.setAttribute("maxlength", "4");
+    heightDiv.appendChild(inchesInput);
+
+    submitButton.addEventListener("click", startImperial);
+  }
+};
+
+const startMetric = () => {
+  console.log("started Metric");
+
+  class PersonMetric {
     constructor(name, height, weight) {
       this.name = name;
       this.height = height;
       this.weight = weight;
     }
     calcBmi = () => {
+      console.log(Math.floor(this.weight / (this.height * this.height)));
       return Math.floor(this.weight / (this.height * this.height));
     };
   }
+
   let textLocation = document.createElement("p");
-  const a = new Person(nameInput, heightInput, weightInput);
+  const a = new PersonMetric(
+    nameInput.value,
+    heightInput.value,
+    weightInput.value
+  );
   textLocation.innerText = `${a.name}, your height is: ${
     Math.floor(a.height * 100) / 100
   }m, your weight is: ${
@@ -26,25 +66,39 @@ const start = () => {
   outputDiv.appendChild(textLocation);
 };
 
-/*const mark = new Person("Mark", "1.69", "78");
-mark.calcBmi();
-console.log(
-  `${mark.name} is ${mark.height}m tall, and weighs ${mark.weight}kg`
-);
-const john = new Person("John", "1.95", "92");
-john.calcBmi();
-console.log(
-  `${john.name} is ${john.height}m tall, and weighs ${john.weight}kg`
-);
+const startImperial = () => {
+  console.log("started Imperial");
+  const heightInputInches = document.getElementById("inches");
 
-const mark2 = new Person("Mark2", "1.88", "95");
-mark2.calcBmi();
-console.log(
-  `${mark2.name} is ${mark2.height}m tall, and weighs ${mark2.weight}kg`
-);
-const john2 = new Person("John2", "1.76", "85");
-john2.calcBmi();
-console.log(
-  `${john2.name} is ${john2.height}m tall, and weighs ${john2.weight}kg`
-);
-*/
+  class PersonImperial {
+    constructor(name, heightFt, heightInches, weight) {
+      this.name = name;
+      this.heightFt = heightFt;
+      this.heightInches = heightInches;
+      this.weight = weight;
+    }
+    calcBmi = () => {
+      let ftConverted = this.heightFt * 12;
+      console.log(ftConverted);
+      let addInchesAndFt = parseInt(ftConverted) + parseInt(this.heightInches);
+      console.log(addInchesAndFt);
+      return Math.floor(
+        (this.weight / (addInchesAndFt * addInchesAndFt)) * 703
+      );
+    };
+  }
+
+  let textLocation = document.createElement("p");
+  const a = new PersonImperial(
+    nameInput.value,
+    heightInput.value,
+    heightInputInches.value,
+    weightInput.value
+  );
+  textLocation.innerText = `${a.name}, your height is: ${Math.floor(
+    a.heightFt
+  )} ft ${heightInputInches.value} inches, your weight is: ${
+    Math.floor(a.weight * 100) / 100
+  }lbs, based off of the data you've given, your BMi is: ${a.calcBmi()}`;
+  outputDiv.appendChild(textLocation);
+};
